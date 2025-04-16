1f9bb25e576573bcd91fe8bc527ada99b485efb0
Balaji7855 <balajip1612@gmail.com>
2025-04-16 11:11:40 +0530

    footer added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+            <h1>footer</h1>
+            <h1>footer</h1>
         </div>
     );
 };
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Carousel from '../Components/Carousel.jsx';
 import Highlights from '../Components/Highlights.jsx';
+import Footer from '../Components/Footer.jsx';
 
 const Homepage = () => {
     return (
         <div>
             <Carousel/>
             <Highlights/>
+            <Footer/>
         </div>
     );
 };
